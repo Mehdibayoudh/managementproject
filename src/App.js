200a8771bf6638cced6ac5470588fb9537822c4c
“Mehdibayoudh” <almehdi.bayoudh@esprit.tn>
2024-02-22 23:09:39 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./component/Header";
+import Sidebar from "./Sidebar";
+ import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Task from "./component/task";
+import Login from "./component/Login";
+import Dashboard from "./component/Dashboard";
+
 function App() {
   return (
-   <Header/>
+<>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Sidebar />}>
+              {/* Nested route for Task */}
+              <Route path="task" element={<Task />} />
+              <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+
+      </Routes>
+
+</>
   );
 }
 
